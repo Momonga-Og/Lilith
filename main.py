315001068e4e2dbfdb2df61a3ed701d0897b711b
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-12-17 16:34:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import asyncio
 import os
-from soundcloud_dl import SoundcloudDL
+from soundcloud import Client
 
 # Bot setup
 intents = discord.Intents.default()
