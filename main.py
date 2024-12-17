bcbccf58d6a7f921fcc00ef15d2137edea094cda
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-12-17 18:26:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import asyncio
 import requests
 import logging
 import uuid
-
+import os 
 # Set up logging to log every step the bot does
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
